a0fbb3a9ef28937e5f3358674ec6c233d6752ec1
Darryl-win <logossoudarryl20@gmail.com>
2024-07-29 13:57:13 +0000

    textfield component

---
+++ lib/components/my_text_filed.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MyTextFiled extends StatelessWidget {
+  final bool obscureText;
+  final String hintText;
+  final TextEditingController controller;
+
+  const MyTextFiled({
+    super.key,
+    required this.hintText,
+    required this.obscureText,
+    required this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: Theme.of(context).colorScheme.tertiary),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: Theme.of(context).colorScheme.primary),
+          ),
+          hintText: hintText,
+          hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
+        ),
+      ),
+    );
+  }
+}
