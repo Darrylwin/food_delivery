152e5b2a9f4c42aa15f7b4ab2b8144d3365bc2e7
Darryl-win <logossoudarryl20@gmail.com>
2024-07-29 14:00:32 +0000

    register page

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/components/my_button.dart';
+import 'package:food_delivery/components/my_text_filed.dart';
+
+class RegisterPage extends StatefulWidget {
+  final void Function()? onTap;
+
+  RegisterPage({
+    super.key,
+    required this.onTap,
+  });
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  // text editing controllers
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //logo
+          Center(
+            child: Icon(
+              Icons.lock_open_rounded,
+              size: 100,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+          ),
+
+          const SizedBox(height: 25),
+
+          // app welcome message
+          Text(
+            "A new member ! Let's Create an account",
+            style: TextStyle(
+              fontSize: 16,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+          ),
+
+          const SizedBox(height: 25),
+
+          // email textfield
+          MyTextFiled(
+            controller: emailController,
+            hintText: "Email",
+            obscureText: false,
+          ),
+
+          const SizedBox(height: 20),
+
+          //password textfield
+          MyTextFiled(
+            controller: passwordController,
+            hintText: "Password",
+            obscureText: true,
+          ),
+
+          const SizedBox(
+            height: 20,
+          ),
+
+          // confirm password textfield
+          MyTextFiled(
+            controller: confirmPasswordController,
+            hintText: "Confirm Password",
+            obscureText: true,
+          ),
+
+          const SizedBox(
+            height: 20,
+          ),
+
+          // sign up button
+          MyButton(
+            onTap: () {},
+            text: "Sign up",
+          ),
+
+          const SizedBox(
+            height: 25,
+          ),
+
+          // already havea an account ?
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Already have an account ?",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              GestureDetector(
+                onTap: widget.onTap,
+                child: Text(
+                  "Login now",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
