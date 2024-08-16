520789500360ece3019f4ac13da6ead5ae721fed
Darryl-win L. <logossoudarryl20@gmail.com>
2024-08-16 23:49:48 +0000

    ui components

---
+++ lib/components/my_current_location.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyCurrentLocation extends StatelessWidget {
+  const MyCurrentLocation({super.key});
+
+  void openLocationSearchBox(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Your Location"),
+        content: TextField(
+          decoration: const InputDecoration(hintText: "Search address ..."),
+        ),
+        actions: [
+          // cancel
+          IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: Icon(Icons.cancel),
+          ),
+
+          //save
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.send),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Deliver now",
+            style: TextStyle(color: Theme.of(context).colorScheme.primary),
+          ),
+          GestureDetector(
+            onTap: () => openLocationSearchBox(context),
+            child: Row(
+              children: [
+                //address
+                Text(
+                  "7007 Street of Vaulence, Miami",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                ),
+
+                // drop down menu
+                Icon(Icons.keyboard_arrow_down_rounded),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/components/my_sliver_app_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class MySliverAppBar extends StatelessWidget {
+  final Widget title;
+  final Widget child;
+
+  const MySliverAppBar({
+    super.key,
+    required this.child,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 340,
+      collapsedHeight: 120,
+      floating: false,
+      pinned: true,
+      actions: [
+        // cart button
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.shopping_cart),
+        ),
+      ],
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+      title: const Text(
+        "Sunset Diner",
+      ),
+      centerTitle: true,
+      flexibleSpace: FlexibleSpaceBar(
+        background: Padding(
+          padding: const EdgeInsets.only(bottom: 50.0),
+          child: child,
+        ),
+        title: title,
+        centerTitle: true,
+        titlePadding: const EdgeInsets.only(left: 0, top: 0),
+        expandedTitleScale: 1,
+      ),
+    );
+  }
+}
