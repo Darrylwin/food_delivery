cf639d9358ab1926f699fe9885a25774329c6d8c
Darryl-win L. <logossoudarryl20@gmail.com>
2024-08-16 23:50:22 +0000

    homepage design

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/components/my_current_location.dart';
 import 'package:food_delivery/components/my_drawer.dart';
+import 'package:food_delivery/components/my_sliver_app_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,14 +9,39 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          "Home",
-        ),
-        backgroundColor: Theme.of(context).colorScheme.surface,
-      ),
+      // appBar: AppBar(
+      //   centerTitle: true,
+      //   title: const Text(
+      //     "Home",
+      //   ),
+      //   backgroundColor: Theme.of(context).colorScheme.surface,
+      // ),
       drawer: MyDrawer(),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySliverAppBar(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Divider(
+                  indent: 25,
+                  endIndent: 25,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+
+                // my curent location
+                MyCurrentLocation(),
+
+                // description box
+              ],
+            ),
+            title: Text('Title'),
+          ),
+        ],
+        body: Container(
+          color: Colors.blue,
+        ),
+      ),
     );
   }
 }
