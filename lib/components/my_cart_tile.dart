bc399d8020395a63316b2a1121976b45e7afb8de
Darrylwin <logossoudarryl20@gmail.com>
2024-11-24 14:01:40 +0000

    elements of cart_page

---
+++ lib/components/my_cart_tile.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/components/my_quantity_selector.dart';
+import 'package:food_delivery/models/cart_item.dart';
+import 'package:food_delivery/models/restaurant.dart';
+import 'package:provider/provider.dart';
+
+class MyCartTile extends StatelessWidget {
+  MyCartTile({super.key, required this.cartItem});
+
+  final CartItem cartItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Restaurant>(
+      builder: (context, restaurant, child) => Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  //food image
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.network(
+                      cartItem.food.imagePath,
+                      height: 100,
+                      // width: 100,
+                    ),
+                  ),
+
+                  const SizedBox(width: 10),
+
+                  //name and price
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //food name
+                      Text(cartItem.food.name),
+
+                      //food price
+                      Text("\$ ${cartItem.food.price}"),
+                    ],
+                  ),
+
+                  const Spacer(),
+
+                  // increment or decrement quantity
+                  MyQuantitySelector(
+                    quantity: cartItem.quantity,
+                    food: cartItem.food,
+                    onIncrement: () => restaurant.addToCart(
+                        cartItem.food, cartItem.selectedAddons),
+                    onDecrement: () => restaurant.removeFromCart(cartItem),
+                  ),
+                ],
+              ),
+            ),
+
+            //addons
+            SizedBox(
+              height: cartItem.selectedAddons.isEmpty ? 0 : 60,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: cartItem.selectedAddons
+                    .map(
+                      (addon) => FilterChip(
+                        label: Row(
+                          children: [
+                            // addon name
+                            Text(addon.name),
+
+                            //addon price
+
+                            Text('(\$${addon.price})'),
+                          ],
+                        ),
+                        
+                        onSelected: (value) {},
+                        backgroundColor:
+                            Theme.of(context).colorScheme.secondary,
+                        labelStyle: TextStyle(
+                          color: Theme.of(context).colorScheme.inversePrimary,
+                          fontSize: 12,
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
