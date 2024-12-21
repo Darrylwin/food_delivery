0c1139e943d78146a51a2012340e36f865b13de0
Darrylwin <logossoudarryl20@gmail.com>
2024-12-21 18:06:25 +0000

    add custom bottom navigation bar to DeliveryProgressPage for driver interaction

--- lib/pages/delivery_progress_page.dart
+++ lib/pages/delivery_progress_page.dart
@@ -11,9 +11,70 @@ class DeliveryProgressPage extends StatelessWidget {
         title: const Text("Delivery in progress"),
         backgroundColor: Colors.transparent,
       ),
-      body: const Column(
+      bottomNavigationBar: _buildBottomNavigationBar(context),
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [
+            MyReceipt(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Custom button Nav Bar for Message/Call the driver
+  Widget _buildBottomNavigationBar(BuildContext context) {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.secondary,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(40),
+          topRight: Radius.circular(40),
+        ),
+      ),
+      padding: const EdgeInsets.all(25),
+      child: Row(
         children: [
-          MyReceipt(),
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.surface,
+              shape: BoxShape.circle,
+            ),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.person_2_rounded,
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+
+          //driver details
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Flash McQueen',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+              Text(
+                'Driver',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+            ],
+          )
+
+          //message button
+          
+
+          //call button
         ],
       ),
     );
