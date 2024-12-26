f5f825459138dbee166c25138a37454332bd791f
Darrylwin <logossoudarryl20@gmail.com>
2024-12-26 11:05:18 +0000

    Add SearchTextField component for enhanced search functionality

---
+++ lib/components/search_text_field.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class SearchTextField extends StatelessWidget {
+  SearchTextField({
+    super.key,
+    required this.controller,
+  });
+  TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: TextField(
+        controller: controller,
+        cursorColor: const Color(0xFF666a6d), // Ajout de la couleur du curseur
+        decoration: InputDecoration(
+          prefixIcon: const Icon(
+            Icons.search_rounded,
+            color: Color(0xFF666a6d),
+          ),
+          suffixIcon: const Icon(
+            Icons.filter_list,
+            color: Color(0xff575b5e),
+          ),
+          filled: true,
+          hintText: 'Search...',
+          hintStyle: const TextStyle(
+            color: Color(0xFFacaeb4),
+          ),
+          fillColor: const Color(0xFFf9f9f9),
+          focusColor: const Color(0xFFf9f9f9),
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.transparent),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.transparent),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          border: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.transparent),
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+      ),
+    );
+  }
+}
