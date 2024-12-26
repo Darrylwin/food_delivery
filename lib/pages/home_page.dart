8a74d2edf02b7475993bb073ad63c554bda4f697
Darrylwin <logossoudarryl20@gmail.com>
2024-12-26 11:05:35 +0000

    Integrate SearchTextField component into HomePage for enhanced search capability

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,10 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:food_delivery/components/my_current_location.dart';
-import 'package:food_delivery/components/my_description_box.dart';
 import 'package:food_delivery/components/my_drawer.dart';
 import 'package:food_delivery/components/my_food_tile.dart';
 import 'package:food_delivery/components/my_sliver_app_bar.dart';
 import 'package:food_delivery/components/my_tab_bar.dart';
+import 'package:food_delivery/components/search_text_field.dart';
 import 'package:food_delivery/models/food.dart';
 import 'package:food_delivery/pages/food_page.dart';
 import 'package:provider/provider.dart';
@@ -23,7 +22,8 @@ class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
   late TabController _tabsController;
   int _bottomNavBarIndex = 0;
-  GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+  final GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+  TextEditingController controller = TextEditingController();
 
   @override
   void initState() {
@@ -71,32 +71,16 @@ class _HomePageState extends State<HomePage>
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: const Color(0xff0d5ef9),
-        title: const Text(
-          'Home',
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
       drawer: const MyDrawer(),
       body: NestedScrollView(
         headerSliverBuilder: (context, innerBoxIsScrolled) => [
           MySliverAppBar(
             title: MyTabBar(tabsController: _tabsController),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
+            child: Column(mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Divider(
-                  indent: 25,
-                  endIndent: 25,
-                  color: Theme.of(context).colorScheme.secondary,
+                SearchTextField(
+                  controller: controller,
                 ),
-
-                // my current location
-                const MyCurrentLocation(),
-
-                // description box
-                const MyDescriptionBox(),
               ],
             ),
           ),
@@ -111,28 +95,28 @@ class _HomePageState extends State<HomePage>
       bottomNavigationBar: CurvedNavigationBar(
         key: _bottomNavigationKey,
         color: Colors.transparent,
-        buttonBackgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        buttonBackgroundColor: Theme.of(context).colorScheme.surface,
         // color: Colors.transparent,
         backgroundColor: Colors.transparent,
         items: [
           Image.asset(
             'assets/icons/home.png',
-            color: Theme.of(context).colorScheme.primary,
+            // color: Theme.of(context).colorScheme.primary,
             height: 25,
           ),
           Image.asset(
             'assets/icons/order.png',
-            color: Theme.of(context).colorScheme.primary,
+            // color: Theme.of(context).colorScheme.primary,
             height: 25,
           ),
           Image.asset(
             'assets/icons/profil.png',
-            color: Theme.of(context).colorScheme.primary,
+            // color: Theme.of(context).colorScheme.primary,
             height: 25,
           ),
           Image.asset(
             'assets/icons/message.png',
-            color: Theme.of(context).colorScheme.primary,
+            // color: Theme.of(context).colorScheme.primary,
             height: 25,
           ),
         ],
