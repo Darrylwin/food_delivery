c679cc60632781c4585bce144be73baa7931d416
Darrylwin <logossoudarryl20@gmail.com>
2024-12-27 15:30:37 +0000

    Refactor SearchTextField to enhance styling and add shadow effect for improved UI

--- lib/components/search_text_field.dart
+++ lib/components/search_text_field.dart
@@ -13,36 +13,53 @@ class SearchTextField extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
-      child: TextField(
-        controller: controller,
-        cursorColor: const Color(0xFF666a6d), // Ajout de la couleur du curseur
-        decoration: InputDecoration(
-          prefixIcon: const Icon(
-            Icons.search_rounded,
-            color: Color(0xFF666a6d),
-          ),
-          suffixIcon:this.sort ? const Icon(
-            Icons.filter_list,
-            color: Color(0xff575b5e),
-          ) : null,
-          filled: true,
-          hintText: 'Search...',
-          hintStyle: const TextStyle(
-            color: Color(0xFFacaeb4),
-          ),
-          fillColor: const Color(0xFFf9f9f9),
-          focusColor: const Color(0xFFf9f9f9),
-          enabledBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.transparent),
-            borderRadius: BorderRadius.circular(30),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.transparent),
-            borderRadius: BorderRadius.circular(30),
-          ),
-          border: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.transparent),
-            borderRadius: BorderRadius.circular(30),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: const Color(0xFFf9f9f9),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.25), // Augmentation de l'opacité de 0.2 à 0.25
+              spreadRadius: 3, // Augmentation du spreadRadius de 2 à 3
+              blurRadius: 10,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: TextField(
+          controller: controller,
+          cursorColor:
+              const Color(0xFF666a6d), // Ajout de la couleur du curseur
+          decoration: InputDecoration(
+            prefixIcon: const Icon(
+              Icons.search_rounded,
+              color: Color(0xFF666a6d),
+            ),
+            suffixIcon: sort
+                ? const Icon(
+                    Icons.filter_list,
+                    color: Color(0xff575b5e),
+                  )
+                : null,
+            filled: true,
+            hintText: 'Search...',
+            hintStyle: const TextStyle(
+              color: Color(0xFFacaeb4),
+            ),
+            fillColor: const Color(0xFFf9f9f9),
+            focusColor: const Color(0xFFf9f9f9),
+            enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.transparent),
+              borderRadius: BorderRadius.circular(30),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.transparent),
+              borderRadius: BorderRadius.circular(30),
+            ),
+            border: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.transparent),
+              borderRadius: BorderRadius.circular(30),
+            ),
           ),
         ),
       ),
