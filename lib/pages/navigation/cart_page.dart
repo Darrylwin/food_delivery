66b700045fb701f974dac34085645f27e13222ad
Darrylwin <logossoudarryl20@gmail.com>
2024-12-27 13:47:01 +0000

    Fix formatting of alert dialog title in CartPage

--- lib/pages/navigation/cart_page.dart
+++ lib/pages/navigation/cart_page.dart
@@ -28,7 +28,8 @@ class CartPage extends StatelessWidget {
                   showDialog(
                     context: context,
                     builder: (context) => AlertDialog(
-                      title: const Text("Are you sure you want to clear the cart ?"),
+                      title: const Text(
+                          "Are you sure you want to clear the cart ?"),
                       actions: [
                         //cancel button
                         TextButton(
