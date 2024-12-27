baf41c8e62cd8378aad90c7d13d0e04ffc5b6c80
Darrylwin <logossoudarryl20@gmail.com>
2024-12-27 17:51:42 +0000

    faire en sorte que mêmequand on scrolle la couleure de la appbar ne change pas (à caue du widget NestedScrollView)

--- lib/pages/navigation/cart_page.dart
+++ lib/pages/navigation/cart_page.dart
@@ -16,97 +16,110 @@ class CartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<Restaurant>(
       builder: (context, restaurant, child) {
-        //cart
         final userCart = restaurant.cart;
 
-        //scaffold ui
         return Scaffold(
           drawer: const MyDrawer(),
-          appBar: AppBar(
-            automaticallyImplyLeading: false,
-            backgroundColor: Colors.white,
-            centerTitle: true,
-            title: const Text(
-              'Order details',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 17,
+          backgroundColor: Colors.white,
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(kToolbarHeight),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 1,
+                    blurRadius: 8,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
               ),
-            ),
-            leading: GestureDetector(
-              onTap: () {
-                Scaffold.of(context).openDrawer();
-              },
-              child: Container(
-                margin: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.3),
-                      spreadRadius: 1,
-                      blurRadius: 8,
-                      offset: const Offset(0, 2),
-                    ),
-                  ],
+              child: AppBar(
+                automaticallyImplyLeading: false,
+                backgroundColor: Colors.white,
+                elevation: 0,
+                centerTitle: true,
+                title: const Text(
+                  'Order details',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17,
+                  ),
                 ),
-                child: const Icon(
-                  Icons.menu_rounded,
-                  size: 22.5,
+                leading: GestureDetector(
+                  onTap: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 1,
+                          blurRadius: 8,
+                          offset: const Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: const Icon(
+                      Icons.menu_rounded,
+                      size: 22.5,
+                    ),
+                  ),
                 ),
               ),
             ),
           ),
-          body: Container(
-            color: Colors.white,
-            child: Column(
-              children: [
-                SearchTextField(
-                  controller: controller,
-                  sort: false,
-                ),
-                //list of cart
-                Expanded(
-                  child: Column(
-                    children: [
-                      userCart.isEmpty
-                          ? const Expanded(
-                              child: Center(
-                                child: Text("Cart is empty"),
-                              ),
-                            )
-                          : Expanded(
-                              child: ListView.builder(
-                                itemCount: userCart.length,
-                                itemBuilder: (context, index) {
-                                  //get individual cart item
-                                  final cartItem = userCart[index];
-
-                                  return MyCartTile(cartItem: cartItem);
-                                },
-                              ),
+          body: Column(
+            children: [
+              SearchTextField(
+                controller: controller,
+                sort: false,
+              ),
+              //list of cart
+              Expanded(
+                child: Column(
+                  children: [
+                    userCart.isEmpty
+                        ? const Expanded(
+                            child: Center(
+                              child: Text("Cart is empty"),
                             ),
-                    ],
-                  ),
-                ),
+                          )
+                        : Expanded(
+                            child: ListView.builder(
+                              itemCount: userCart.length,
+                              itemBuilder: (context, index) {
+                                //get individual cart item
+                                final cartItem = userCart[index];
 
-                //button to pay
-                MyButton(
-                  text: 'PLACE MY ORDER',
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const PayementPage(),
-                      ),
-                    );
-                  },
+                                return MyCartTile(cartItem: cartItem);
+                              },
+                            ),
+                          ),
+                  ],
                 ),
+              ),
 
-                const SizedBox(height: 25),
-              ],
-            ),
+              //button to pay
+              MyButton(
+                text: 'PLACE MY ORDER',
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const PayementPage(),
+                    ),
+                  );
+                },
+              ),
+
+              const SizedBox(height: 25),
+            ],
           ),
         );
       },
