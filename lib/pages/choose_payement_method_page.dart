039ad28051c033838e64cbf155a29a55888d2eb6
Darrylwin <logossoudarryl20@gmail.com>
2024-12-28 09:29:38 +0000

    refactor ChoosePaymentMethodPage to include dynamic payment calculations and improve navigation flow

--- lib/pages/choose_payement_method_page.dart
+++ lib/pages/choose_payement_method_page.dart
@@ -2,113 +2,169 @@ import 'package:flutter/material.dart';
 import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 import 'package:food_delivery/components/choose_payement_method.dart';
 import 'package:food_delivery/components/my_button.dart';
+import 'package:food_delivery/models/restaurant.dart';
+import 'package:provider/provider.dart';
+
+import 'delivery_progress_page.dart';
 
 class ChoosePayementMethodPage extends StatelessWidget {
   const ChoosePayementMethodPage({super.key});
 
-  void Function()? paypalPayement(BuildContext context) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const PaypalCheckoutView(
-          clientId: '',
-          
-
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // automaticallyImplyLeading: false,
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: const Text(
-          'Payement method',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 17,
-          ),
-        ),
-        leading: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Container(
-            margin: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.3),
-                  spreadRadius: 1,
-                  blurRadius: 8,
-                  offset: const Offset(0, 2),
-                ),
-              ],
+    return Consumer<Restaurant>(
+      builder: (context, restaurant, child) {
+        double discount = (restaurant.getTotalPrice() * 3.75) / 100;
+        double total = restaurant.getTotalPrice() - discount;
+
+        void Function()? paypalPayement(BuildContext context) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => PaypalCheckoutView(
+                clientId: '',
+                secretKey: '',
+                transactions: [
+                  {
+                    'amount': {
+                      'total': total.toStringAsFixed(2),
+                      'currency': 'USD',
+                      'details': {
+                        'subtotal': total.toStringAsFixed(2),
+                        'shipping': "3.75%",
+                        'shipping_discount': discount,
+                      },
+                    },
+                    'description': 'Pay with Paypal',
+                    'items_list': const {
+                      'items': [
+                        {
+                          "name": "Apple",
+                          "quantity": 4,
+                          "price": '5',
+                          "currency": "USD"
+                        },
+                        {
+                          "name": "Pineapple",
+                          "quantity": 5,
+                          "price": '10',
+                          "currency": "USD"
+                        },
+                      ],
+                    },
+                  },
+                ],
+                note: 'Contact us for any question on your order',
+                onSuccess: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const DeliveryProgressPage(),
+                    ),
+                  );
+                },
+                onError: (error) {
+                  print("onError: $error");
+                  Navigator.pop(context);
+                },
+                onCancel: () {
+                  print('cancelled:');
+                },
+              ),
             ),
-            child: const Icon(
-              Icons.arrow_back_ios_new,
-              size: 22.5,
+          );
+        }
+
+        return Scaffold(
+          appBar: AppBar(
+            // automaticallyImplyLeading: false,
+            backgroundColor: Colors.white,
+            elevation: 0,
+            title: const Text(
+              'Payement method',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 17,
+              ),
+            ),
+            leading: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+                margin: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 1,
+                      blurRadius: 8,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: const Icon(
+                  Icons.arrow_back_ios_new,
+                  size: 22.5,
+                ),
+              ),
             ),
           ),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 7),
-                child: ChoosePayementMethod(
-                  icon: 'assets/icons/paypal.png',
-                  onTap: paypalPayement(context),
-                ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 7),
+                    child: ChoosePayementMethod(
+                      icon: 'assets/icons/paypal.png',
+                      onTap: paypalPayement(context),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 7),
+                    child: ChoosePayementMethod(
+                      icon: 'assets/icons/master_card.png',
+                      onTap: () {},
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: MyButton(
+                      text: 'ADD CARD +',
+                      onTap: () {},
+                      color: const Color.fromARGB(255, 201, 233, 255)
+                          .withOpacity(.8),
+                      textColor: const Color(0xff0d5ef9),
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  const Text(
+                    'Other methods',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 13,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 7),
+                    child: ChoosePayementMethod(
+                      icon: 'assets/icons/cash.png',
+                      onTap: () {},
+                    ),
+                  ),
+                ],
               ),
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 7),
-                child: ChoosePayementMethod(
-                  icon: 'assets/icons/master_card.png',
-                  onTap: () {},
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child: MyButton(
-                  text: 'ADD CARD +',
-                  onTap: () {},
-                  color:
-                      const Color.fromARGB(255, 201, 233, 255).withOpacity(.8),
-                  textColor: const Color(0xff0d5ef9),
-                ),
-              ),
-              const SizedBox(height: 15),
-              const Text(
-                'Other methods',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 13,
-                ),
-              ),
-              const SizedBox(height: 15),
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 7),
-                child: ChoosePayementMethod(
-                  icon: 'assets/icons/cash.png',
-                  onTap: () {},
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
