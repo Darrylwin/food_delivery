0c7e3f648d011b02091b268712e892293070b9b3
Darrylwin <logossoudarryl20@gmail.com>
2024-12-28 01:42:33 +0000

    refactor MyButton; add customizable color and textColor properties for improved flexibility

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 class MyButton extends StatelessWidget {
   final Function()? onTap;
   final String text;
+  final Color color;
+  final Color textColor;
 
   const MyButton({
     super.key,
+    this.color = const Color(0xff0d5ef9),
+    this.textColor = Colors.white,
     required this.text,
     required this.onTap,
   });
@@ -18,15 +22,14 @@ class MyButton extends StatelessWidget {
         padding: const EdgeInsets.all(13),
         margin: const EdgeInsets.symmetric(horizontal: 25),
         decoration: BoxDecoration(
-        
-          color: const Color(0xff0d5ef9),
+          color: color,
           borderRadius: BorderRadius.circular(27),
         ),
         child: Center(
           child: Text(
             text,
-            style: const TextStyle(
-              color: Colors.white,
+            style: TextStyle(
+              color: textColor,
               fontWeight: FontWeight.w500,
               fontSize: 16,
             ),
