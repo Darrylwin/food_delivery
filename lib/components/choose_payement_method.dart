aa76beb13f74087de99e396424593819d43e6741
Darrylwin <logossoudarryl20@gmail.com>
2024-12-28 08:57:49 +0000

    add onTap functionality to ChoosePaymentMethod widget for improved interactivity

--- lib/components/choose_payement_method.dart
+++ lib/components/choose_payement_method.dart
@@ -1,49 +1,56 @@
 import 'package:flutter/material.dart';
 
 class ChoosePayementMethod extends StatelessWidget {
-  const ChoosePayementMethod({
+  ChoosePayementMethod({
     super.key,
     required this.icon,
+    required this.onTap,
   });
+
   final String icon;
+  void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.3),
-            spreadRadius: .7,
-            blurRadius: 5,
-            offset: const Offset(2, 2), // changé pour ombre à droite et en bas
-          ),
-        ],
-        borderRadius: BorderRadius.circular(18),
-      ),
-      child: Row(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: Image.asset(
-              icon,
-              height: 35,
-              width: 35,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              spreadRadius: .7,
+              blurRadius: 5,
+              offset:
+                  const Offset(2, 2), // changé pour ombre à droite et en bas
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              icon,
-              style: const TextStyle(
-                fontWeight: FontWeight.w400,
-                fontSize: 14,
+          ],
+          borderRadius: BorderRadius.circular(18),
+        ),
+        child: Row(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: Image.asset(
+                icon,
+                height: 35,
+                width: 35,
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                icon,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
