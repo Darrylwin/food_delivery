37064fe50df7ad830b42374b6cfaf55012156775
Darrylwin <logossoudarryl20@gmail.com>
2025-01-01 15:34:19 +0000

    DeliverTo page

---
+++ lib/pages/deliver_to_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/components/location.dart';
+import 'package:food_delivery/components/my_button.dart';
+import 'package:food_delivery/components/total.dart';
+import 'package:food_delivery/pages/delivery_progress_page.dart';
+
+class DeliverToPage extends StatelessWidget {
+  const DeliverToPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text(
+          'Deliver To',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 17,
+          ),
+        ),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Container(
+            margin: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: 1,
+                  blurRadius: 8,
+                  offset: const Offset(0, 2),
+                ),
+              ],
+            ),
+            child: const Icon(
+              Icons.arrow_back_ios_new,
+              size: 22.5,
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
+        child: Column(
+          children: [
+            Location(
+              place: 'Home',
+              address: '123 Main Street, Mundo',
+            ),
+            Location(
+              place: 'Office',
+              address: '123 Main Street, Za warudo',
+            ),
+            const Spacer(),
+            const Total(),
+            const SizedBox(height: 8),
+            MyButton(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const DeliveryProgressPage(),
+                  ),
+                );
+              },
+              text: 'NEXT',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
