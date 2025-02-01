dad8052b31140cef4a304c498e171a9e9ce5842a
Darrylwin <logossoudarryl20@gmail.com>
2025-02-01 20:34:57 +0000

    init notif services

---
+++ lib/services/notifications/notif_service.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotifService {
+  final notificationPlugin = FlutterLocalNotificationsPlugin();
+
+  bool _isInitialized = false;
+
+  bool get isInitialised => _isInitialized;
+
+//INITIALIZE
+  Future<void> initNotifiaction() async {
+    if (_isInitialized) return;
+
+    //prepare android innit settings
+    const initSettingAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    //prepare ios init settings
+    const initSettingsIOS = DarwinInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+    );
+
+    //init settings
+    const initSettings = InitializationSettings(
+      android: initSettingAndroid,
+      iOS: initSettingsIOS,
+    )
+
+    //Initialize the plugin
+    await notificationPlugin.initialize(initSettings);
+  }
+//NOTIFICATION DETAIL SETUP
+
+//SHOW NOTIFICATION
+
+//ON NOTI TAP
+}
