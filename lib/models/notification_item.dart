19c6fe9d3a05ff5503cdf811331d943f9a1e3e10
Darrylwin <logossoudarryl20@gmail.com>
2025-02-02 10:51:46 +0000

    feat: add NotificationItem model and NotificationProvider for managing notifications

---
+++ lib/models/notification_item.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class NotificationItem {
+  final String title;
+  final String description;
+  final String time;
+
+  NotificationItem({
+    required this.title,
+    required this.description,
+    required this.time,
+  });
+}
+
+class NotificationProvider extends ChangeNotifier {
+  final List<NotificationItem> _notifications = [];
+
+  List<NotificationItem> get notifications => _notifications;
+
+  void addNotification(NotificationItem notification) {
+    _notifications.insert(0, notification);
+    notifyListeners();
+  }
+}
