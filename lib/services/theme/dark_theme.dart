79893d44fc133733f0fbf4954a3998c9816783c2
Darrylwin <logossoudarryl20@gmail.com>
2025-03-03 00:40:29 +0000

    update themes colors

--- lib/services/theme/dark_theme.dart
+++ lib/services/theme/dark_theme.dart
@@ -1,50 +1,46 @@
 import 'package:flutter/material.dart';
 
 ThemeData darkMode = ThemeData(
-   brightness: Brightness.dark,
-    scaffoldBackgroundColor: const Color(0xFF1C1F26), // Couleur du fond
-    primaryColor: Colors.white,
-    colorScheme: const ColorScheme.dark(
-      primary: Colors.white,
-      secondary: Color(0xFF0D5EF9), // Bouton bleu
-      surface: Color(0xFF2C3039), // Couleur des cartes
+  brightness: Brightness.dark,
+  scaffoldBackgroundColor: const Color(0xFF181E22), // Couleur du fond
+  primaryColor: Colors.white,
+  colorScheme: const ColorScheme.dark(
+    primary: Colors.white,
+    secondary: Color(0xFF0D5EF9), // Bouton bleu
+    tertiary:  Color.fromARGB(255, 215, 214, 214),
+    surface: Color(0xFF2C3039), // Couleur des cartes
+  ),
+  // cardTheme: const CardTheme(
+  //   color: Color(0xFF2C3039),
+  //   elevation: 4,
+  // ),
+  appBarTheme: const AppBarTheme(
+    color: Color(0xFF181E22),
+    elevation: 0,
+  ),
+  bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+    backgroundColor: Color(0xFF181E22),
+    selectedItemColor: Colors.white,
+    unselectedItemColor: Color(0xff94979F),
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    filled: true,
+    fillColor: const Color(0xFF2C3039),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(30),
+      borderSide: BorderSide.none,
     ),
-    cardTheme: const CardTheme(
-      color: Color(0xFF2C3039),
-      elevation: 4,
-    ),
-    appBarTheme: const AppBarTheme(
-      color: Color(0xFF1C1F26),
-      elevation: 0,
-    ),
-    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-      backgroundColor: Color(0xFF1C1F26),
-      selectedItemColor: Colors.white,
-      unselectedItemColor: Colors.grey,
-    ),
-    inputDecorationTheme: InputDecorationTheme(
-      filled: true,
-      fillColor: const Color(0xFF2C3039),
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(30),
-        borderSide: BorderSide.none,
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: const Color(0xFF0D5EF9), // Couleur du bouton bleu
+      foregroundColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
       ),
     ),
-    textTheme: const TextTheme(
-      titleLarge: TextStyle(color: Colors.white, fontWeight: FontWeight.bold), // "Popular Menu"
-      titleMedium: TextStyle(color: Colors.white, fontWeight: FontWeight.bold), // Nom des plats
-      bodySmall: TextStyle(color: Colors.grey), // Nom des restaurants
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: const Color(0xFF0D5EF9), // Couleur du bouton bleu
-        foregroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8),
-        ),
-      ),
-    ),
-    iconTheme: const IconThemeData(
-      color: Colors.white,
-    ),
-  );
+  ),
+  iconTheme: const IconThemeData(
+    color: Colors.white,
+  ),
+);
--- lib/services/theme/light_theme.dart
+++ lib/services/theme/light_theme.dart
@@ -1,50 +1,46 @@
 import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
-    brightness: Brightness.light,
-    scaffoldBackgroundColor: Colors.white,
-    primaryColor: Colors.black,
-    colorScheme: const ColorScheme.light(
-      primary: Colors.black,
-      secondary: Color(0xFF0D5EF9), // Bouton bleu
-      surface: Colors.white, // Couleur des cartes
+  brightness: Brightness.light,
+  scaffoldBackgroundColor: Colors.white,
+  primaryColor: Colors.black,
+  colorScheme: const ColorScheme.light(
+    primary: Color(0xff181E22),
+    secondary: Color(0xFF0D5EF9), // Bouton bleu
+    tertiary: Color.fromARGB(255, 52, 51, 51),
+    surface: Color.fromARGB(255, 254, 237, 237), // Couleur des cartes
+  ),
+  // cardTheme: const CardTheme(
+  //   color: Colors.white,
+  //   elevation: 2,
+  // ),
+  appBarTheme: const AppBarTheme(
+    color: Colors.white,
+    elevation: 0,
+  ),
+  bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+    backgroundColor: Colors.white,
+    selectedItemColor: Color(0xff28303F),
+    unselectedItemColor: Color(0xff94979F),
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    filled: true,
+    fillColor: const Color(0xFFF9F9F9),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(30),
+      borderSide: BorderSide.none,
     ),
-    cardTheme: const CardTheme(
-      color: Colors.white,
-      elevation: 2,
-    ),
-    appBarTheme: const AppBarTheme(
-      color: Colors.white,
-      elevation: 0,
-    ),
-    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-      backgroundColor: Colors.white,
-      selectedItemColor: Colors.black,
-      unselectedItemColor: Colors.grey,
-    ),
-    inputDecorationTheme: InputDecorationTheme(
-      filled: true,
-      fillColor: const Color(0xFFF5F5F5),
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(30),
-        borderSide: BorderSide.none,
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: const Color(0xFF0D5EF9), // Couleur du bouton bleu
+      foregroundColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
       ),
     ),
-    textTheme: const TextTheme(
-      titleLarge: TextStyle(color: Colors.black, fontWeight: FontWeight.bold), // "Popular Menu"
-      titleMedium: TextStyle(color: Colors.black, fontWeight: FontWeight.bold), // Nom des plats
-      bodySmall: TextStyle(color: Colors.grey), // Nom des restaurants
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: const Color(0xFF0D5EF9), // Couleur du bouton bleu
-        foregroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8),
-        ),
-      ),
-    ),
-    iconTheme: const IconThemeData(
-      color: Colors.black,
-    ),
-  );
+  ),
+  iconTheme: const IconThemeData(
+    color: Color(0xff292D32),
+  ),
+);
